10d87e520cb4ff0d73623009849b173f0baaebc4
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-30 09:16:31 +0200

    added abstract class Clothes

---
+++ src/main/java/se/sti/Clothes.java
@@ -0,0 +1,18 @@
+package se.sti;
+
+public abstract class Clothes {
+
+    protected double price;
+
+    public Clothes(double price){
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
