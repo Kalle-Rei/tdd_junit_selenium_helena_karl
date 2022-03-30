43e990b80d0633c39ad277bc0ea38bcc5219e92e
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-03-30 09:52:20 +0200

    First test added

--- src/test/java/se/sti/ShoppingBasketTest.java
+++ src/test/java/se/sti/ShoppingBasketTest.java
@@ -1,12 +1,27 @@
 package se.sti;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ShoppingBasketTest {
 
+@Test
+    public void doesConstructorWork() {
+    Clothes clothes = new Shirt(150,"M","Red");
+    Assertions.assertEquals(150, clothes.getPrice());
 
-    
-    //
+}
+
+@Test
+    public void doesConstructorWorkHere() {
+    Clothes clothes = new Trousers(125, "38", true);
+    Assertions.assertEquals(125, clothes.getPrice());
+
+
+
+}
+
+
 
 
 
