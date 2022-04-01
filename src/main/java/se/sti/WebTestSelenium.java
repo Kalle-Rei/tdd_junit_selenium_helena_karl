4c536677473b1df29ead650a6102042230629287
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-04-01 09:03:10 +0200

    added imports for chrome as well

--- src/main/java/se/sti/WebTestSelenium.java
+++ src/main/java/se/sti/WebTestSelenium.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
